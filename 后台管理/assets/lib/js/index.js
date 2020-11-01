10057ec5ae99349550671a86a0037c58a64033c1
zengwenlongstar <1658451483@qq.com>
2020-11-01 13:45:09 +0800

    提交

--- 后台管理/assets/lib/js/index.js
+++ 后台管理/assets/lib/js/index.js
@@ -6,14 +6,40 @@ $(function() {
 function getUserInfo() {
     $.ajax({
         url: '/my/userinfo',
-        headers: {
-            Authorization: localStorage.getItem('token') || ''
-        },
+
         success: function(res) {
             if (res.status !== 0) {
                 return layui.layer.msg('请求失败')
             }
-
+            renDer(res.data)
+            console.log(res);
         }
     })
 }
+
+
+
+
+function renDer(user) {
+    let use = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + use)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        let fistr = use[0].toUpperCase()
+        console.log(fistr);
+        $('.text-avattar').html(fistr).show()
+    }
+}
+
+$('#btn').on('click', function() {
+
+    layer.confirm('确认退出登录?', { icon: 3, title: '提示' }, function(index) {
+        //do something
+        localStorage.removeItem('token')
+        location.href = '/login.html'
+        layer.close(index);
+    });
+})
