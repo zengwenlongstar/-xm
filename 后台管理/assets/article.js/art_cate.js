53aa142f2a01cf4e065cf9e20dfacc62c2d9e8c5
zengwenlongstar <1658451483@qq.com>
2020-11-02 17:57:32 +0800

    提交

--- 后台管理/assets/article.js/art_cate.js
+++ 后台管理/assets/article.js/art_cate.js
@@ -1,4 +1,6 @@
 $(function() {
+    let layer = layui.layer
+    let form = layui.form;
     initArtCateLis()
 
     function initArtCateLis() {
@@ -10,4 +12,92 @@ $(function() {
             }
         })
     }
+    var open = null;
+    $('#btnAddCate').on('click', function() {
+        open = layer.open({
+            type: 1,
+            anim: 2,
+            area: ['500px', '250px'],
+            title: '添加文章类别',
+            content: $('#dialog-add').html()
+        });
+    })
+
+    $('body').on('submit', '#boxAddCate', function(e) {
+        e.preventDefault()
+        $.ajax({
+            type: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('添加文章列表失败')
+                }
+                initArtCateLis()
+                layer.msg('添加文章列表成功')
+                layer.close(open)
+            }
+        })
+    })
+
+
+
+
+    var indexEdit = null
+    $('tbody').on('click', '.btn-edit', function() {
+        // 弹出一个修改文章分类信息的层
+        indexEdit = layer.open({
+            type: 1,
+            anim: 2,
+            area: ['500px', '250px'],
+            title: '修改文章分类',
+            content: $('#dialog-edit').html()
+        })
+
+        var id = $(this).attr('data-id')
+            // 发起请求获取对应分类的数据
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates/' + id,
+            success: function(res) {
+                form.val('form-edit', res.data)
+            }
+        })
+    })
+
+
+    $('body').on('submit', '#form-edit', function(e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/updatecate',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('编辑文章列表失败')
+                }
+                layer.msg('编辑文章列表成功')
+                layer.close(indexEdit)
+                initArtCateLis()
+            }
+        })
+    })
+
+
+    $('tbody').on('click', '.btn-delete', function() {
+        var id = $(this).attr('data-id')
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
+            $.ajax({
+                url: '/my/article/deletecate/' + id,
+                success: function(res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除列表失败')
+                    }
+                    layer.msg('删除列表成功')
+                    layer.close(index)
+                    initArtCateLis()
+                }
+            })
+        })
+    })
 })
---
+++ 后台管理/assets/article.js/art_list.js
@@ -0,0 +1,52 @@
+$(function() {
+
+    var q = {
+        pagenum: 1, // 页码值，默认请求第一页的数据
+        pagesize: 2, // 每页显示几条数据，默认每页显示2条
+        cate_id: '', // 文章分类的 Id
+        state: '' // 文章的发布状态
+    }
+
+    template.defaults.imports.dataFormat = function(date) {
+        const dt = new Date(date)
+
+        var y = dt.getFullYear()
+        var m = padZero(dt.getMonth() + 1)
+        var d = padZero(dt.getDate())
+
+        var hh = padZero(dt.getHours())
+        var mm = padZero(dt.getMinutes())
+        var ss = padZero(dt.getSeconds())
+
+        return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+    }
+
+    // 定义补零的函数
+    function padZero(n) {
+        return n > 9 ? n : '0' + n
+    }
+    initTable()
+
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function(res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('获取文章列表失败！')
+                }
+                // 使用模板引擎渲染页面的数据
+                var htmlStr = template('tpl-table', {
+                    data: [
+                        { Id: 28476, title: "济南", pub_date: "2020-10-29 23:04:33.407", state: "已发布", cate_name: "改我头条试试" },
+                        { Id: 28477, title: "呢喃1", pub_date: "2020-10-29 20:28:47.326", state: "已发布", cate_name: "改我头条试试" }
+                    ]
+                });
+                $('tbody').html(htmlStr)
+                    // 调用渲染分页的方法
+            }
+        })
+    }
+})
